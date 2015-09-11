e60023bd1a07c800be8bce3ba11d6c799a3f12a1
Kim Kling <kim.kling@hotmail.com>
2015-09-11 19:12:47 +0200

    Cleaned up

--- app/init.js
+++ app/init.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require('body-parser');
-var forceSSL = require('express-force-ssl');
 var authenticate = require('./middlewares/authenticate');
 var morgan = require('morgan');
 
@@ -11,15 +10,8 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-if(process.env.TLS_ENABLE === 'true' && process.env.APP_ENV !== 'dev'){
-    //app.use(forceSSL); Disabled until SSL is working properly
-}
 
 app.use(authenticate);
-
-// View configuration
-//app.set("views", __dirname);
-app.set("view engine", "jade");
 
 // Bind routers to resources
 app.use(require('./routes'));
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,29 +1,6 @@
 
 var utils = {};
 
-utils.checkNewDocumentAgainstSchema = function(document, schema){
-    var rules = schema.dataRules;
-    for(var i = 0; i < rules.length; i++){
-        var rule = rules[i];
-
-        // Check for undefined property, if so assign standard value and then done
-        if(typeof document.data[rule.name] === 'undefined'){
-            document.data[rule.name] = rule.standardvalue;
-        }else{
-            // If value exist, begin matching
-
-            // Check for correct type on property
-            if(rule.type){
-                if(typeof document.data[rule.name] !== rule.type){
-                    return false;
-                }
-            }
-
-        }
-    }
-    return true;
-};
-
 utils.checkDocumentAgainstSchema = function(document, schema){
     var rules = schema.dataRules;
     for(var i = 0; i < rules.length; i++){
@@ -33,12 +10,12 @@ utils.checkDocumentAgainstSchema = function(document, schema){
         if(typeof document.data[rule.name] === 'undefined'){
             document.data[rule.name] = rule.standardvalue;
         }else{
-            console.log("Checking rules for: " + rule.name);
+            //console.log("Checking rules for: " + rule.name);
             // If value exist, begin matching
 
             // Check for correct type on property
             if(rule.type){
-                console.log("Data property type: " + typeof document.data[rule.name] + " rule needs " + rule.type + " OR " + rule.standardvalue);
+                //console.log("Data property type: " + typeof document.data[rule.name] + " rule needs " + rule.type + " OR " + rule.standardvalue);
                 if(typeof document.data[rule.name] !== rule.type && document.data[rule.name] !== rule.standardvalue){
                     return false;
                 }
