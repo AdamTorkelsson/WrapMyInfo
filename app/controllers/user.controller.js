16a61bf7d4a4b775fc2bd9c84cf3e52e895b10b7
Kim Kling <kim.kling@hotmail.com>
2015-09-11 13:42:40 +0200

    Document validation

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,11 +1,12 @@
 var models = require('../models');
 var wmiCrypto = require("../utils/wmi-crypto");
 var errors = require("../utils/errors");
+var utils = require("../utils/utils");
 
 var userController = {};
 // Helpers
 
-var verifyDocument = function(req, func){
+var verifyDocument = function(req, res, func){
     models.Document.findById(req.params.document, {
         include: [models.Blob]
     }).then(function(document){
@@ -82,7 +83,7 @@ userController.getDocuments = function(req, res){
         }
     }).then(function(documents){
         for(var i = 0; i < documents.length; i++){
-            documents[i].data = wmiCrypto.decryptData(documents[i].data);
+            documents[i].data = wmiCrypto.decryptAsObject(documents[i].data);
         }
         res.json(documents);
         //TODO: Enable fetching Blobs with req.query.includeBlobs
@@ -90,38 +91,133 @@ userController.getDocuments = function(req, res){
 };
 
 userController.postDocument = function(req, res){
-    models.Document.create({
-        UserId: req.params.user,
-        SchemaId: req.params.schema,
-        meta: req.body.meta,
-        data: wmiCrypto.encryptData(req.body.data)
-    }).then(function(document){
-        document.data = req.body.data;
-        res.json(document);
+    var developer;
+    var user;
+    if(req.authenticated.type === "developer"){
+        developer = req.authenticated.entity;
+    }else{
+        user = req.authenticated.entity;
+    }
+
+    models.Schema.findOne({
+        where: {
+            id: req.params.schema,
+            DeveloperId: (developer ? developer.id : user.Developer.id)
+        }
+    }).then(function(schema){
+        if(!schema){
+            res.json({
+                status: "Error",
+                message: "Specified Schema can not be found"
+            });
+        }
+        if(!req.body.data){
+            res.json({
+                status: "Error",
+                message: "No data object provided"
+            });
+        }
+
+        var document = models.Document.build({
+            UserId: req.params.user,
+            SchemaId: req.params.schema,
+            meta: req.body.meta,
+            data: req.body.data
+        });
+        if(utils.checkDocumentAgainstSchema(document, schema)){
+            var plainData = document.data;
+            document.dataValues.data = wmiCrypto.encryptObject(plainData);
+            document.save().then(function(document){
+                document.dataValues.data = plainData;
+                res.json(document);
+            });
+        }else{
+            res.json({
+                status: "Error",
+                message: "Document is not valid according to the Schema"
+            });
+        }
     });
+
+
 };
 
 userController.getDocument = function(req, res){
-    verifyDocument(req, function(document){
-        document.data = wmiCrypto.decryptData(document.data);
+    verifyDocument(req, res, function(document){
+        //TODO: Verify and update document to latest schema version
+        document.data = wmiCrypto.decryptAsObject(document.data);
         res.json(document);
     });
 };
 
 userController.putDocument = function(req, res){
-    verifyDocument(req, function(document){
-        //TODO: Verify structure against Schema, and replace if it matches.
-        document.data = wmiCrypto.encryptData(req.body.data);
-        document.meta = req.body.meta;
-        document.save().then(function(savedDocument){
-            savedDocument.data = req.body.data;
-            res.json(savedDocument)
+    verifyDocument(req, res, function(document){
+
+        var developer;
+        var user;
+        if(req.authenticated.type === "developer"){
+            developer = req.authenticated.entity;
+        }else{
+            user = req.authenticated.entity;
+        }
+
+        models.Schema.findOne({
+            where: {
+                id: req.params.schema,
+                DeveloperId: (developer ? developer.id : user.Developer.id)
+            }
+        }).then(function(schema){
+            if(document && schema) { // These variables need to be set
+                //TODO: Add a way to remove properties in meta and uncontrolled properties in data
+
+                document.data = wmiCrypto.decryptAsObject(document.data);
+
+                //console.log("### Document before update: " + JSON.stringify(document));
+                // Perform changes and additions to Document.data
+                for (var dataProperty in req.body.data) {
+                    if (req.body.data.hasOwnProperty(dataProperty)) {
+                        //console.log("### Coping propertycontent to Document: " + dataProperty);
+                        document.data[dataProperty] = req.body.data[dataProperty];
+                    }
+                }
+
+                // Perform changes and additions to Document.meta
+                for (var metaProperty in req.body.meta) {
+                    if (req.body.meta.hasOwnProperty(metaProperty)) {
+                        //console.log("### Coping propertycontent to Document: " + metaProperty);
+                        document.meta[metaProperty] = req.body.meta[metaProperty];
+                    }
+                }
+
+                //console.log("### Document after update: " + JSON.stringify(document));
+
+                //TODO: Verify that document is valid according to Schema
+                if (utils.checkDocumentAgainstSchema(document, schema)) {
+
+                    var plainData = document.data;
+                    document.data = wmiCrypto.encryptObject(plainData);
+                    document.save().then(function (savedDocument) {
+                        savedDocument.data = plainData; // Set plain data to return to developer
+                        res.json(savedDocument)
+                    });
+                } else { // Document did not match Schema
+                    res.json({
+                        status: "Error",
+                        message: "Document is not valid according to the Schema"
+                    });
+                }
+            }else{ // document or schema was falsy
+                res.json({
+                    status: "Error",
+                    message: "Specified Document or Schema can not be found"
+                });
+            }
         });
     });
 };
 
 userController.deleteDocument = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         document.destroy().then(function(){
             res.json({
                 status: "success"
@@ -131,21 +227,21 @@ userController.deleteDocument = function(req, res){
 };
 
 userController.getBlobs = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         var blobs = document.Blobs;
         for(var i = 0; i < blobs.length; i++){
-            blobs[i].blob = wmiCrypto.decryptData(blobs[i].blob);
+            blobs[i].blob = wmiCrypto.decryptText(blobs[i].blob);
         }
         res.json(document.Blobs);
     });
 };
 
 userController.postBlob = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         models.Blob.create({
             DocumentId: document.id,
             meta: req.body.meta,
-            blob: wmiCrypto.encryptData(req.body.blob)
+            blob: wmiCrypto.encryptText(req.body.blob)
         }).then(function(blob){
             blob.blob = req.body.blob;
             res.json(blob);
@@ -154,7 +250,7 @@ userController.postBlob = function(req, res){
 };
 
 userController.getBlob = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         models.Blob.findById(req.params.blob).then(function(blob){
             if(!blob){
                 res.json({
@@ -173,7 +269,7 @@ userController.getBlob = function(req, res){
 };
 
 userController.putBlob = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         models.Blob.findById(req.params.blob).then(function(blob){
             if(!blob){
                 res.json({
@@ -185,7 +281,7 @@ userController.putBlob = function(req, res){
                 }); //TODO: Make error messages better.
             }else{
                 blob.meta = req.body.meta;
-                blob.blob = wmiCrypto.encryptData(req.body.blob);
+                blob.blob = wmiCrypto.encryptObject(req.body.blob);
                 blob.save().then(function(savedBlob){
                     savedBlob.blob = req.body.blob;
                     res.json(savedBlob);
@@ -196,7 +292,7 @@ userController.putBlob = function(req, res){
 };
 
 userController.deleteBlob = function(req, res){
-    verifyDocument(req, function(document){
+    verifyDocument(req, res, function(document){
         models.Blob.findById(req.params.blob).then(function(blob){
             if(!blob){
                 res.json({
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,4 +1,52 @@
 
 var utils = {};
 
+utils.checkNewDocumentAgainstSchema = function(document, schema){
+    var rules = schema.dataRules;
+    for(var i = 0; i < rules.length; i++){
+        var rule = rules[i];
+
+        // Check for undefined property, if so assign standard value and then done
+        if(typeof document.data[rule.name] === 'undefined'){
+            document.data[rule.name] = rule.standardvalue;
+        }else{
+            // If value exist, begin matching
+
+            // Check for correct type on property
+            if(rule.type){
+                if(typeof document.data[rule.name] !== rule.type){
+                    return false;
+                }
+            }
+
+        }
+    }
+    return true;
+};
+
+utils.checkDocumentAgainstSchema = function(document, schema){
+    var rules = schema.dataRules;
+    for(var i = 0; i < rules.length; i++){
+        var rule = rules[i];
+
+        // Check for undefined property, if so assign standard value and then done
+        if(typeof document.data[rule.name] === 'undefined'){
+            document.data[rule.name] = rule.standardvalue;
+        }else{
+            console.log("Checking rules for: " + rule.name);
+            // If value exist, begin matching
+
+            // Check for correct type on property
+            if(rule.type){
+                console.log("Data property type: " + typeof document.data[rule.name] + " rule needs " + rule.type + " OR " + rule.standardvalue);
+                if(typeof document.data[rule.name] !== rule.type && document.data[rule.name] !== rule.standardvalue){
+                    return false;
+                }
+            }
+
+        }
+    }
+    return true;
+};
+
 module.exports = utils;
